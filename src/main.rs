8676b25809649ed665f5e5c41c76a3d64d1472bb
fwcd <fwcdmail@gmail.com>
2019-08-22 16:20:24 +0200

    Basic audio playback using rodio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate rodio;
+
+use std::fs::File;
+use std::io::BufReader;
+use rodio::Source;
+use rodio::Sink;
+
 fn main() {
-	println!("Hello world!");
+	let device = rodio::default_output_device().unwrap();
+	let file = File::open("local/oldtownroad.mp3").unwrap();
+	let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+	let sink = Sink::new(&device);
+	sink.append(source);
+	sink.sleep_until_end();
 }
