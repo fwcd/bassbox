d4ce1635328f801575614e3bb68710a9c5aaec2b
fwcd <fwcdmail@gmail.com>
2019-09-05 22:18:14 +0200

    Make lowpass filter length dynamic

--- src/processing/filter.rs
+++ src/processing/filter.rs
@@ -2,6 +2,7 @@
 
 use crate::audioformat::{StandardFrame, empty_standard_frame};
 use dsp::sample::Frame;
+use std::collections::VecDeque;
 
 /// A facility that processes audio on a frame-by-frame
 /// basis, possibly maintaining state across calls.
@@ -12,19 +13,24 @@ pub trait Filter {
 
 /// A filter that only lets low frequencies pass through.
 pub struct LowpassFilter {
-	last: StandardFrame
+	last: VecDeque<StandardFrame>,
+	length: usize
 }
 
 impl LowpassFilter {
 	pub fn new() -> LowpassFilter {
-		LowpassFilter { last: empty_standard_frame() }
+		let length: usize = 5;
+		LowpassFilter { last: VecDeque::with_capacity(length), length: length }
 	}
 }
 
 impl Filter for LowpassFilter {
 	fn apply(&mut self, frame: StandardFrame) -> StandardFrame {
-		let out = self.last.add_amp(frame);
-		self.last = frame;
-		return out.scale_amp(0.5);
+		if self.last.len() == self.length {
+			self.last.pop_front();
+		}
+		self.last.push_back(frame);
+		let out = self.last.iter().fold(empty_standard_frame(), |a, b| a.add_amp(*b));
+		return out.scale_amp(1.0 / (self.length as f32));
 	}
 }
