de075d7e07afffd31738508a71715ebee3d7708b
fwcd <fwcdmail@gmail.com>
2019-09-23 01:21:40 +0200

    Use reentrant mutex from parking-lot for shared graph

--- core/src/graph.rs
+++ core/src/graph.rs
@@ -1,5 +1,6 @@
 use dsp::Node;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use parking_lot::ReentrantMutex;
 use crate::audioformat::StandardFrame;
 use crate::util::empty::Empty;
 
@@ -10,7 +11,7 @@ pub type AudioGraph<N> = dsp::Graph<StandardFrame, N>;
 /// The audio graph which is shared between
 /// an engine and possibly control threads
 /// (such as RPC-mechanisms).
-pub type SharedAudioGraph<N> = Arc<Mutex<AudioGraph<N>>>;
+pub type SharedAudioGraph<N> = Arc<ReentrantMutex<AudioGraph<N>>>;
 
 /// Creates a new, shared atomically reference-counted
 /// audio graph with a single, empty master node.
@@ -18,5 +19,5 @@ pub fn new_shared_graph<N>() -> SharedAudioGraph<N> where N: Empty + Node<Standa
 	let mut graph = AudioGraph::new();
 	let master = graph.add_node(N::empty());
 	graph.set_master(Some(master));
-	Arc::new(Mutex::new(graph))
+	Arc::new(ReentrantMutex::new(graph))
 }
